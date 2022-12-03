2a51569d455a9c9c564cb95a04ecd0a4a54c1e8a
Pedro Henrique <pprocha179@gmail.com>
2022-12-03 16:00:01 -0300

    Initial Commit

---
+++ DinoSauloApp/DinoSauloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinoSauloAppApp.swift
+//  DinoSauloApp
+//
+//  Created by Pedro Henrique Pinto Rcoha on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DinoSauloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
